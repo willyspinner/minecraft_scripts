a80544b9c1506c0412ca7603e878f64a4ae44064
willyspinner <wilsonjusuf1998@gmail.com>
2021-03-16 22:44:59 -0700

    more color pads

--- pic2block.py
+++ pic2block.py
@@ -1,6 +1,7 @@
 # Given image, fill blocks
 import cv2
 import numpy as np
+import scipy
 from automation_utils import fill_block
 import json
 
@@ -11,14 +12,20 @@ with open('./block_map.json') as f:
         blocks_map[i]['color'] = np.array(blocks_map[i]['color'])
 
 
+def get_distance(rgb1, rgb2):
+    # can't use cosine distance because magnitude MATTERS!
+    # e.g. 1,1,1 and 100,100,100 are treated as the same, even though they're not.
+    dist = np.linalg.norm(rgb1 - rgb2, ord=2)
+    return dist
+
+
 def find_block_by_color(rgb):
     global blocks_map
     # find nearest color
-    print("Finding block for rgb", rgb)
     min_dist = float('inf')
     min_block_id = None
     for block_item in blocks_map:
-        dist = np.linalg.norm(rgb - block_item['color'], ord=2)
+        dist = get_distance(rgb, block_item['color'])
         if dist < min_dist:
             min_dist = dist
             min_block_id = block_item['id']
@@ -34,7 +41,8 @@ def write_image_blocks(image_path, starting_coords):
       for j in range(cols):
           pixel = img[i,j]
           nearest_block = find_block_by_color(pixel)
-          fill_block(nearest_block, x_0 + i, y_0 + j, z_0)
+
+          fill_block(nearest_block, x_0 + j, y_0 + rows - 1 - i, z_0)
 if __name__ == "__main__":
-    write_image_blocks('/Users/willyspinner/Desktop/royce.png', [10, 10, 10])
+    write_image_blocks('./willysilly-contr.png', [ -41, 78, -2780])
 
